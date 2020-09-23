db9b71ffd40cccc79baa37552893b27009a2f3a0
Luiz Carlos <d71k1n@live.com>
2020-09-23 09:27:16 -0300

    add_docker_101

---
+++ docker/dockerizing_101/frontend/index.js
@@ -0,0 +1,14 @@
+async function getApi() {
+    await axios.get('http://api:3000/')
+        .then(function (response) {
+            // handle success
+            console.log(response);
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        })
+        .then(function () {
+            // always executed
+        });
+}
